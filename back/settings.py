dc3963aa440f0f7ae54cfb4371df311234d2db63
Igwe Ugochukwu Michael Sylvester <96844092+elinteerie@users.noreply.github.com>
2024-03-21 02:00:40 +0100

    changed to True

--- back/settings.py
+++ back/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t&w-vlg1id8c%-gpao$s17o3yua1h)d0b$x5t*_76r#_-c4x9q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['airdroptask.onrender.com', '.onrender.com', 'drewtoken.com', 'www.drewtoken.com', 'localhost', '127.0.0.1',]
 
